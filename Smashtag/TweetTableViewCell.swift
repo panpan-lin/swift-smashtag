06182d1b0c4442b7a6a6885279798109db5844c4
Panpan Lin <panpanli@ie.ibm.com>
2016-11-03 15:07:40 +0000

    check in new table cell layout

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Panpan Lin on 03/11/2016.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+
+
+}
