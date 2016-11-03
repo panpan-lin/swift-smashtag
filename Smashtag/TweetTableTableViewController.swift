77d2d15b7ff532e294de608a2b4873be99ad46b9
Panpan Lin <panpanli@ie.ibm.com>
2016-11-03 13:13:58 +0000

    initial checkin

---
+++ Smashtag/TweetTableTableViewController.swift
@@ -0,0 +1,93 @@
+//
+//  TweetTableTableViewController.swift
+//  Smashtag
+//
+//  Created by Panpan Lin on 03/11/2016.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableTableViewController: UITableViewController {
+
+    var tweets = [Array<Twitter.Tweet>]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    var searchText: String? {
+        didSet {
+            tweets.removeAll()
+            searchForTweets()
+            title = searchText
+        }
+    }
+    
+    private var twitterRequest: Twitter.Request? {
+        if let query = searchText , !query.isEmpty {
+            return Twitter.Request(search: query + "-filter:retweets", count: 100)
+        }
+        return nil
+    }
+    
+    private var lastTwitterRequest: Twitter.Request?
+    
+    private func searchForTweets() {
+        if let request = twitterRequest {
+            lastTwitterRequest = request
+            request.fetchTweets{ [weak weakSelf = self] newTweets in
+                DispatchQueue.main.async {
+                    if request == weakSelf?.lastTwitterRequest {
+                        if !(newTweets.isEmpty) {
+                             weakSelf?.tweets.insert(newTweets, at: 0)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchText = "#stanford"
+    }
+
+    // MARK: - UITableViewDataSource
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return tweets.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tweets[section].count
+    }
+    
+    private struct Storyboard {
+        static let TweetCellIdentifier = "Tweet"
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.TweetCellIdentifier, for: indexPath)
+
+        let tweet = tweets[indexPath.section][indexPath.row]
+        cell.textLabel?.text = tweet.text
+        cell.detailTextLabel?.text = tweet.user.name
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
